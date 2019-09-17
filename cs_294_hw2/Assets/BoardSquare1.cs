760454c2f5d52e981593ba37e1d3cf0e13d401aa
Xi Chen <xichen1167@gmail.com>
2019-09-17 00:23:12 -0700

    commited hw folder

---
+++ cs_294_hw2/Assets/BoardSquare1.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardSquare1 : MonoBehaviour, OnTouch3D
+{
+    public GameObject cube;
+    public GameObject sphere;
+
+    public GameObject gameController;
+
+    public float debounceTime = 0.3f;
+    public int cubeNum =0;
+    public int cubeSide;
+    //public string playerSide;
+    //public bool thisSet = false;
+    // Stores a counter for the current remaining wait time.
+    private float remainingDebounceTime;
+    private GameController gameControllerScript;
+    //private GameController gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        remainingDebounceTime = 0;
+        gameControllerScript = gameController.GetComponent<GameController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (remainingDebounceTime > 0)
+            remainingDebounceTime -= Time.deltaTime;
+
+    }
+
+    public void OnTouch()
+    {
+        // If a touch is found and we are not waiting,
+        if (remainingDebounceTime <= 0 )
+        {
+            // thisSet = true;
+            // Move the object up by 10cm and reset the wait counter.
+            //this.gameObject.transform.Translate(new Vector3(0, 0.1f, 0));
+            remainingDebounceTime = debounceTime;
+            // cube1.gameObject.SetActive(true);
+            
+            cubeSide = gameControllerScript.GetPlayerSide();
+
+            if (cubeSide == 1){
+                cube.gameObject.SetActive(true);
+                gameControllerScript.placePiece(cubeNum, cubeSide);
+            }
+            else{
+                sphere.gameObject.SetActive(true);
+                gameControllerScript.placePiece(cubeNum, cubeSide);
+            }
+
+            gameControllerScript.EndTurn();
+
+        }
+    }
+
+
+}
---
+++ cs_294_hw2/Assets/GameController.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+	public int[] cubeList;
+	public GameObject gameOverPanel;
+	public Text gameOverText;
+	public GameObject restartButton;
+
+	public GameObject[] cubes;
+	public GameObject[] spheres;
+
+	private int playerSide;
+	private int moves;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	playerSide = 1;
+    	moves = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void placePiece(int spot, int side){
+    	cubeList[spot] = side;
+
+    }
+
+    public int GetPlayerSide(){
+    	return playerSide;
+    }
+
+    /*
+    public void placeRandomPiece(){
+
+    	for (int i = 0; i < cubeList.Length; i++){
+    		if (cubeList[i] == 0) {
+    			int randomSpot = i;
+    		}
+    	}
+    	
+    	cubeList[randomSpot] = 2;
+    	cubeObjectList[randomSpot].gameObject.SetActive(true);
+    	
+    	//if(!cubeObjectList[0].transform.parent.gameObject.GetComponent<BoardSquare1>().thisSet)
+    	//	Debug.Log("test");
+    }
+    */
+
+    public void EndTurn(){
+    	moves++;
+
+    	if ( cubeList[0] == playerSide && cubeList[1] == playerSide && cubeList[2] == playerSide)
+        {
+            GameOver();
+        }
+        if (cubeList [3] == playerSide && cubeList [4] == playerSide && cubeList [5] == playerSide)
+        {
+            GameOver();
+        }
+
+        if (cubeList [6] == playerSide && cubeList [7] == playerSide && cubeList [8] == playerSide)
+        {
+            GameOver();
+        }
+
+        if (cubeList [0] == playerSide && cubeList [3] == playerSide && cubeList [6] == playerSide)
+        {
+            GameOver();
+        }
+
+        if (cubeList [1] == playerSide && cubeList [4] == playerSide && cubeList [7] == playerSide)
+        {
+            GameOver();
+        }
+
+        if (cubeList [2] == playerSide && cubeList [5] == playerSide && cubeList [8] == playerSide)
+        {
+            GameOver();
+        }
+
+        if (cubeList [0] == playerSide && cubeList [4] == playerSide && cubeList [8] == playerSide)
+        {
+            GameOver();
+        }
+
+        if (cubeList [2] == playerSide && cubeList [4] == playerSide && cubeList [6] == playerSide)
+        {
+            GameOver();
+        }
+
+        if (moves >= 9){
+        	gameOverPanel.SetActive(true);
+	    	gameOverText.text = "It's a tie!";
+	    	restartButton.SetActive(true);
+        }
+
+        ChangeSides();
+    }
+
+    void ChangeSides ()
+    {
+        if (playerSide == 1){
+        	playerSide = 2;
+        }
+        else if (playerSide == 2){
+        	playerSide = 1;
+        }
+    }
+
+    void GameOver(){
+    	gameOverPanel.SetActive(true);
+    	if (playerSide == 1){
+    		gameOverText.text = "Blue Cube Wins!";
+    	}
+    	else{
+    		gameOverText.text = "White Sphere Wins!";
+    	}
+
+    	restartButton.SetActive(true);
+        
+    }
+
+    public void RestartGame ()
+    {
+        playerSide = 1;
+        moves = 0;
+        gameOverPanel.SetActive(false);
+        restartButton.SetActive(false);
+        // SetBoardInteractable(true);
+
+
+        for (int i = 0; i < cubeList.Length; i++)
+        {
+        	if (cubeList[i] == 1){
+        		cubes[i].gameObject.SetActive(false);
+        	}
+        	else{
+        		spheres[i].gameObject.SetActive(false);
+        	}
+
+            cubeList[i] = 0;
+        }
+    }
+
+
+}
